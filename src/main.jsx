f00effde1d433105d0f84d61833c4031fb7eef61
sid177 <siddharthantikekar@gmail.com>
2023-11-27 17:22:48 +0530

    Set app base

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx'
 import './assets/sass/style.scss';
 
-createBrowserRouter(routes, {
-  basename: "/task-manager-ui",
-});
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="task-manager-ui">
     <App />
   </BrowserRouter>
 );
